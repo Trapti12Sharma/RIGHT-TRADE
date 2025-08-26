4835024140dc93357701b4a759249c432a66b519
Aarpit688 <aarpit688@gmail.com>
2025-08-26 12:31:09 +0530

    Updated Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           links: [
             {
               title: "Deposits and Withdrawals",
-              link: "/deposits-and-withdrawals",
+              link: "/deposit-and-withdrawal",
             },
             {
               title: "Trading Commissions and Fees",
